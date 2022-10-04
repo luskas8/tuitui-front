449a82dacd6ed972c391e972a85edcbb843a14c1
luskas8 <gnomo1049@gmail.com>
2022-10-04 12:36:43 -0300

    [ADD] initial configs

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # TuiTui
+
+## Pré requisitos:
+
+- [None LTS](https://nodejs.org/en/)
+- [Eslint Plugin](https://marketplace.visualstudio.com/items?itemName=dbaeumer.vscode-eslint)
+
+### Iniciando:
+
+```sh
+$ npm install
+$ npm run dev
+```
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
+import { defineConfig, splitVendorChunkPlugin } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react(), splitVendorChunkPlugin()],
+  root: 'src',
+  build: {
+    outDir: '../dist',
+  },
+  server: {
+    watch: {
+      usePolling: true,
+    },
+  },
+  publicDir: "public"
 })
