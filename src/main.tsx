a8a29c3183b15ff83d44e27e5afc5854f9917e75
luskas8 <gnomo1049@gmail.com>
2022-11-09 14:33:36 -0300

    antd design, react-router-dom, signin page

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { RouterProvider } from 'react-router-dom'
+import { routes } from './Routes'
+import './styles/global.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-        <h1>TuiTui Front</h1>
+        <RouterProvider router={routes}/>
   </React.StrictMode>
 )
---
+++ src/pages/SignIn.tsx
@@ -0,0 +1,16 @@
+import { Layout } from 'antd'
+import React from 'react'
+
+const { Content, Sider } = Layout
+export function SignIn () {
+  return (
+        <Layout>
+            <Content>
+                formulário
+            </Content>
+            <Sider>
+                foto tuitui
+            </Sider>
+        </Layout>
+  )
+}
