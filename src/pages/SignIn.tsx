d381acd8f2963df54722ac04450c23ca21d3a318
luskas8 <gnomo1049@gmail.com>
2022-11-15 12:23:40 -0300

    tailwind, signin page

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,16 +1,21 @@
-import { Layout } from 'antd'
 import React from 'react'
+import { ReactComponent as Branding } from '../assets/branding/branding.svg'
+import { ReactComponent as Tuitui } from '../assets/branding/tuitui.svg'
 
-const { Content, Sider } = Layout
 export function SignIn () {
   return (
-        <Layout>
-            <Content>
-                formulário
-            </Content>
-            <Sider>
-                foto tuitui
-            </Sider>
-        </Layout>
+        <div className='w-full h-screen flex md:flex-row flex-col'>
+            <div className='md:w-1/2 w-full h-screen flex justify-center items-center md:flex-col bg-white'>
+                <Branding />
+                <h1 className='font-medium text-6xl text-light-black decoration-inherit'>
+                    conectando ideias
+                </h1>
+            </div>
+            <div className='md:w-1/2 w-full flex flex-row md:flex-col justify-center items-center bg-purple p-14'>
+                <div className='max-w-[608px] w-full h-full'>
+                    <Tuitui width="100%" height="100%" />
+                </div>
+            </div>
+        </div>
   )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,21 @@
 import { defineConfig, splitVendorChunkPlugin } from 'vite'
 import react from '@vitejs/plugin-react'
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), splitVendorChunkPlugin()],
+  plugins: [
+    react(),
+    splitVendorChunkPlugin(),
+    svgr(),
+  ],
+  css: {
+    preprocessorOptions: {
+      less: {
+        javascriptEnabled: true,
+      },
+    },
+  },
   root: 'src',
   build: {
     outDir: '../dist',
