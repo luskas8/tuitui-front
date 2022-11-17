bb04b907e55152e4c716133863a83cfa2bba47a6
luskas8 <gnomo1049@gmail.com>
2022-11-17 14:11:44 -0300

    vite-tsconfig-paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig, splitVendorChunkPlugin } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from "vite-plugin-svgr";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,6 +9,7 @@ export default defineConfig({
     react(),
     splitVendorChunkPlugin(),
     svgr(),
+    tsconfigPaths({ root: "../" })
   ],
   css: {
     preprocessorOptions: {
